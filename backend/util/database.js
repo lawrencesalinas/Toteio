0ebdce06b553f544d009398321ab95f425bd3d22
Lawrence <lawrencesalinas@yahoo.com>
2022-07-14 02:25:38 -0400

    deplot test

--- backend/util/database.js
+++ backend/util/database.js
@@ -3,9 +3,14 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-const sequelize = new Sequelize('Tote-io', 'root', process.env.DBPASS, {
-  dialect: 'mysql',
-  host: 'localhost',
-})
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASS,
+  {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.HOST,
+  }
+)
 
 module.exports = sequelize
